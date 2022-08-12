cffd71e3c5f5f6edd55479477d5a17cde5fcd206
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-08-12 12:06:43 -0400

    Funciona dibujar barcos iniciales

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,40 +10,21 @@ export default function Home() {
   const { variables, actions } = useBattleShipContext();
   
 
-  
-
 
   const barquito = new Ship_2(variables.boardUser, []);
 
-  /* useEffect(() => {
-
-    console.log(barquito)
-    barquito.placeShip();
-    barquito.indexes.forEach(x => console.log(x))
-    console.log(barquito.indexes[0])
-    actions.drawShip("user", barquito.indexes[0])
-
-  }, [])
-
-  useEffect(() => {
-    if (barquito.indexes !== []) {
-      [...barquito.indexes].forEach(x => { actions.drawShip("user", x) });
-    }
-  }, []) */
+ 
 
   const startGame = () => {
-    actions.drawShip("user", 0)
-    actions.drawShip("user", 1)
-    actions.hitShip("user", 2)
-    actions.missShip("user", 3)
+    barquito.placeShip();
+    console.log(barquito.indexes)
+    barquito.indexes.forEach(x => {
+      actions.drawShip("user", x)
+    })
 
   }
 
-  useEffect(() => {
-    console.log("hola")
-    
-    
-  }, [variables.boardUser])
+ 
   
 
 
